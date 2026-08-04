859cb249a43214f73e8f1c041420855d03bfacde
njlyon0 <njlyon@alumni.iastate.edu>
2026-08-04 12:24:55 -0400

    style (setup): fully write-out `FALSE`

--- -setup.r
+++ -setup.r
@@ -9,19 +9,19 @@
 ## ---------------------------------- ##
 
 # Make relevant top-level folder
-dir.create(file.path("data"), showWarnings = F)
+dir.create(file.path("data"), showWarnings = FALSE)
 
 # Make sub-folders of 'data'
 ## Order of following lines is a rough match for workflow order
-dir.create(file.path("data", "00_raw"), showWarnings = F)
-dir.create(file.path("data", "01-B_std-structure"), showWarnings = F)
-dir.create(file.path("data", "01-D_std-contents"), showWarnings = F)
-# dir.create(file.path("data", "02-B_wrtds-ready"), showWarnings = F)
-# dir.create(file.path("data", "02-C_wrtds-results"), showWarnings = F)
-# dir.create(file.path("data", "02-D_wrtds-bootstrap"), showWarnings = F)
+dir.create(file.path("data", "00_raw"), showWarnings = FALSE)
+dir.create(file.path("data", "01-B_std-structure"), showWarnings = FALSE)
+dir.create(file.path("data", "01-D_std-contents"), showWarnings = FALSE)
+# dir.create(file.path("data", "02-B_wrtds-ready"), showWarnings = FALSE)
+# dir.create(file.path("data", "02-C_wrtds-results"), showWarnings = FALSE)
+# dir.create(file.path("data", "02-D_wrtds-bootstrap"), showWarnings = FALSE)
 
 # Make a subfolder for diagnostics & tests
-dir.create(file.path("data", "tests"), showWarnings = F)
+dir.create(file.path("data", "tests"), showWarnings = FALSE)
 
 # Clear environment + collect garbage
 rm(list = ls()); gc()
